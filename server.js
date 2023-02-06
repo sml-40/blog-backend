0eddd25e15e93330ce42ec60ea3361b8b96c7ab8
Selina Lavery <selina118@hotmail.com>
2023-02-06 11:26:05 +0000

    add database connection setup in config

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Require DB Configuration File
+const db = require("./config/db");
+
+//Establish Database Connection
+mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
+
 const indexRouter = require("./routes/index");
 
 const app = express();
